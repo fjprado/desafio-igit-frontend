f221c430133748bcc74ef24b238ab01cdb838996
Fernando J Prado <f.j.prado@hotmail.com>
2020-07-09 21:16:37 -0300

    Mudança de ambiente

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
-  headers: {
-    'Content-type': 'application/json',
-  },
+    // baseURL: "http://localhost:8081/",
+    baseURL: "https://grades-igti-backend.herokuapp.com/",
+    headers: {
+        "Content-type": "application/json",
+    },
 });
