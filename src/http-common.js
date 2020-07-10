6a1c45f3bd8b809e7866671c53ebfae42e09e01c
Fernando J Prado <f.j.prado@hotmail.com>
2020-07-09 22:33:47 -0300

    URL Api

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-    // baseURL: "http://localhost:3000/",
-    baseURL: "https://grades-igti-backend.herokuapp.com/",
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         "Content-type": "application/json",
     },
